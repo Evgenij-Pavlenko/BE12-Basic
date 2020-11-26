09269be21e2bb8ed6106398d594038859f4b6be5
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-11-26 21:34:34 +0100

    single number of list

---
+++ tasks/src/main/java/pac20201126/FindPair.java
@@ -0,0 +1,27 @@
+package pac20201126;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+/**
+ * Есть лист целых ( Integer) чисел. Известно что каждое число, кроме одного, имеет пару. Найти число без пары.
+ * {3, 5, 3}->5; {3,5,3,3,3}->5; {3,5,3,5,3}->3
+ */
+public class FindPair {
+
+    public int findPair(List<Integer> listInt) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i : listInt) {
+            map.computeIfPresent(i, (k, v) -> v + 1);
+            map.putIfAbsent(i, 1);
+        }
+        System.out.println(map);
+
+        // O(sqr(n))
+        int ret = map.entrySet().stream().filter(m -> m.getValue() % 2 != 0).map(Map.Entry::getKey).findFirst().get();
+        return ret;
+    }
+}
---
+++ tasks/src/test/java/pac20201126/FindPairTest.java
@@ -0,0 +1,33 @@
+package pac20201126;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *  {3, 5, 3}->5; {3,5,3,3,3}->5; {3,5,3,5,3}->3
+ */
+import static org.junit.jupiter.api.Assertions.*;
+
+class FindPairTest {
+
+    FindPair findPair = new FindPair();
+
+    @Test
+    public void setFindPair_listInteger_5(){
+        List<Integer> accept = Arrays.asList(3,5,3);
+        assertEquals(5, findPair.findPair(accept));
+    }
+    @Test
+    public void setFindPair_bigListInteger_5(){
+        List<Integer> accept = Arrays.asList(3,5,3,3,3);
+        assertEquals(5, findPair.findPair(accept));
+    }
+    @Test
+    public void setFindPair_listIntegerWith3_(){
+        List<Integer> accept = Arrays.asList(3,5,3,5,3);
+        assertEquals(3, findPair.findPair(accept));
+    }
+
+}
