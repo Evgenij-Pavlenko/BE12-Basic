cd6dac5b3b37a9cf91d64c5cbe855c709e9b6270
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2021-01-18 16:43:20 +0100

    or so - data in PostConstruct

--- contacts/src/main/java/com/pavlenko/contacts/config/ContactConfig.java
+++ contacts/src/main/java/com/pavlenko/contacts/config/ContactConfig.java
@@ -2,6 +2,7 @@ package com.pavlenko.contacts.config;
 
 import com.pavlenko.contacts.model.Contact;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -10,27 +11,28 @@ import java.util.Map;
 
 @Configuration
 @EnableWebMvc
+@ComponentScan
 public class ContactConfig {
 
-    @Bean
-    public Contact ivan(){
-        return new Contact(1, "Ivan", "Ivanov", 20);
-    }
-    @Bean
-    public Contact petr(){
-        return new Contact(2, "Petr", "Petrov", 40);
-    }
-    @Bean
-    public Contact john(){
-        return new Contact(3, "John", "Jonson", 30);
-    }
-
+//    @Bean
+//    public Contact ivan(){
+//        return new Contact(1, "Ivan", "Ivanov", 20);
+//    }
+//    @Bean
+//    public Contact petr(){
+//        return new Contact(2, "Petr", "Petrov", 40);
+//    }
+//    @Bean
+//    public Contact john(){
+//        return new Contact(3, "John", "Jonson", 30);
+//    }
+//
     @Bean
     public HashMap<Integer, Contact> contacts(){
         HashMap map = new HashMap<>();
-        map.put(ivan().getId(), ivan());
-        map.put(petr().getId(), petr());
-        map.put(john().getId(), john());
+//        map.put(ivan().getId(), ivan());
+//        map.put(petr().getId(), petr());
+//        map.put(john().getId(), john());
         return map;
     }
 }
--- contacts/src/main/java/com/pavlenko/contacts/repository/InMemoryContactRepo.java
+++ contacts/src/main/java/com/pavlenko/contacts/repository/InMemoryContactRepo.java
@@ -12,10 +12,18 @@ import java.util.stream.Collectors;
 @Repository
 public class InMemoryContactRepo implements IContactRepo {
 
-    Map<Integer,Contact> source;
+    Map<Integer, Contact> source;
 
+    @Autowired
     public InMemoryContactRepo(Map<Integer, Contact> source) {
         this.source = source;
+    }
+
+    @PostConstruct
+    private void loadData(){
+        source.put(1, new Contact(1, "Ivan", "Ivanov", 20));
+        source.put(2, new Contact(2, "Petr", "Petrov", 40));
+        source.put(3, new Contact(3, "John", "Jonson", 30));
     }
 
     @Override
@@ -32,11 +40,11 @@ public class InMemoryContactRepo implements IContactRepo {
     public void save(Contact contact) {
         int lastKey = source.keySet().stream().mapToInt(v -> v).max().getAsInt();
         lastKey++;
-        if (contact.getId()==0){
+        if (contact.getId() == 0) {
             contact.setId(lastKey);
             source.put(lastKey, contact);
-        } else{
-            source.put(contact.getId(),contact);
+        } else {
+            source.put(contact.getId(), contact);
         }
 
     }
