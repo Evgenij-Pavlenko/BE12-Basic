942ce47a274d1625facd398d897e1a31fccfd6e6
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2021-01-28 00:20:36 +0100

    LK Solution

---
+++ tasks/src/test/java/lk20210127/TableSolutionTest.java
@@ -0,0 +1,15 @@
+package lk20210127;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TableSolutionTest {
+    TableSolution ts = new TableSolution();
+
+    @Test
+    public void testSolution(){
+
+    }
+
+}
