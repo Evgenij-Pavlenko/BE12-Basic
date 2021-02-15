86bf7d418b6dcf0103bb2fedae512b9c67183d47
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2021-02-16 00:37:56 +0100

    ctrl + L

--- tasks/src/main/java/hw20210215_range_of_numbers/RangeOfNumbers.java
+++ tasks/src/main/java/hw20210215_range_of_numbers/RangeOfNumbers.java
@@ -6,21 +6,22 @@ package hw20210215_range_of_numbers;
  */
 public class RangeOfNumbers {
 
-    public int getCountNumbers(int a, int b){
+    public int getCountNumbers(int a, int b) {
         int retCount = 0;
-        for (int i = a; i <=b; i++) {
-            if (isOne(i)){
+        for (int i = a; i <= b; i++) {
+            if (isOne(i)) {
                 retCount++;
             }
         }
         return retCount;
     }
-    private boolean isOne(int num){
-        while (num>0){
-            if (num%10==1){
+
+    private boolean isOne(int num) {
+        while (num > 0) {
+            if (num % 10 == 1) {
                 return true;
             }
-            num /=10;
+            num /= 10;
         }
         return false;
     }
