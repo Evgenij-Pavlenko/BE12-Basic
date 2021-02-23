8e9d16ee0e1e05844a65b09fd7ed6b7bba00797e
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2021-02-23 13:34:09 +0100

    1. Solve - return Map<Integer, Integer> update with bad Deque

--- tasks/src/main/java/hw202124_cut_and_set/CutSolve.java
+++ tasks/src/main/java/hw202124_cut_and_set/CutSolve.java
@@ -1,8 +1,6 @@
 package hw202124_cut_and_set;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /*
 1. Есть лист объектов "Отрезок":
@@ -16,7 +14,7 @@ Map<Integer, Integer> solve (List<Cut> cuts, List<Integer> numbers)
  */
 public class CutSolve {
 
-    public Map<Integer, Integer> getNumberSegments(List<Cut> cuts, List<Integer> numbers) {
+    public Map<Integer, Integer> getNumberSegments1(List<Cut> cuts, List<Integer> numbers) {
         Map<Integer, Integer> res = new HashMap<>();
         for (int num : numbers) {
             for (Cut cut : cuts) {
@@ -26,6 +24,26 @@ public class CutSolve {
                 }
             }
         }
+        return res;
+    }
+
+    //как-то через Deque, но что-то не получилось( Забыл, как делали. На уроке посмотрю
+    public Map<Integer, Integer> getNumberSegments(List<Cut> cuts, List<Integer> numbers) {
+        Map<Integer, Integer> res = new HashMap<>();
+        Deque<Cut> cutDeque = new LinkedList<>();
+        int index  = 0;
+        int indexCount = 0;
+for(Cut cut: cuts){
+    int num = numbers.get(index);
+    if (cut.left <= num && cut.right >= num){
+        indexCount++;
+        cutDeque.add(cut);
+    } else{
+        res.put(num, cutDeque.size());
+        index++;
+    }
+
+}
         return res;
     }
 }
